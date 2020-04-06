6a9d399595b6f902e887b5bb07956074faca9cd2
Milton Ocampo <miltonf55@hotmail.com>
2020-04-06 02:37:39 -0500

    Ahora sí, server al 100

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
-const http = require("http").Server(app);
 var app = express();
+const http = require("http").Server(app);
 var bodyParser=require('body-parser');
+var path = require('path');
 
 app.use( bodyParser.json());
 app.use(bodyParser.urlencoded({ 
 	extended:true
 }));
 
-app.set("port", (process.env.PORT || 3001));
+app.set("port", (process.env.PORT || 5555));
 app.use(express.static("public"));  
 
 app.get("/", function(req, res) {
+    
 });
 
 app.get("*", function(req, res) {
@@ -22,7 +24,8 @@ http.listen(app.get("port"), function() {
 	console.log("Servidor en el puerto", app.get("port"));
 });
 
-app.post("Vino", function(req, res) {
+app.post("/Vino", function(req, res) {
 	let css1=req.body.card1;
 	let css2=req.body.card2;
+	res.send("Hola")
 });
